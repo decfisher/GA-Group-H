dc337b375203978bcbc14218bd7c84b8e51a0755
Declan Fisher <declan.fisher01@outlook.com>
2022-02-04 14:51:56 +0000

    Added methods to extract employee information

--- src/main/java/com/napier/sem/Application.java
+++ src/main/java/com/napier/sem/Application.java
@@ -1,8 +1,6 @@
 package com.napier.sem;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Application {
     /**
@@ -13,25 +11,19 @@ public class Application {
     /**
      * Connect to the MySQL database.
      */
-    public void connect()
-    {
-        try
-        {
+    public void connect() {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        int retries = 1000;
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
@@ -39,13 +31,11 @@ public class Application {
                 System.out.println("Successfully connected");
                 break;
             }
-            catch (SQLException sqle)
-            {
+            catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+                sqle.printStackTrace();
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
@@ -54,19 +44,55 @@ public class Application {
     /**
      * Disconnect from the MySQL database.
      */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+
+    public void displayEmployee(Employee emp) {
+        if (emp != null) {
+            System.out.println(emp.emp_no + " "
+                    + emp.first_name + " "
+                    + emp.last_name + "\n"
+                    + emp.title + "\n"
+                    + "Salary:" + emp.salary + "\n"
+                    + emp.dept_name + "\n"
+                    + "Manager: " + emp.manager + "\n");
+        }
+    }
+
+    public Employee getEmployee(int ID) {
+
+        try {
+            Statement s = con.createStatement();
+
+            String select = "SELECT emp_no, first_name, last_name "
+                    + "FROM employees "
+                    + "WHERE emp_no = " + ID;
+
+            ResultSet rset = s.executeQuery(select);
+
+            if(rset.next()) {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+
     }
 }
