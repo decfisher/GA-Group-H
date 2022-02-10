b143c390d729442c97ad3fd645f4bb34537194cb
Declan Fisher <declan.fisher01@outlook.com>
2022-02-10 18:28:36 +0000

    Added Salary Report

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -1,16 +1,14 @@
 package com.napier.sem;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         Application app = new Application();
         app.connect();
-        Employee emp = app.getEmployee(255530);
-        app.displayEmployee(emp);
+        ArrayList<Employee> employees = app.getAllSalaries();
+        System.out.println(employees.size());
         app.disconnect();
     }
 }
